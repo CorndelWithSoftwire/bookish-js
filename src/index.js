d6d32f7f8963f975b07e10c4332059f4aab38797
Dave Artus <djna01@gmail.com>
2021-04-11 06:43:53 +0100

    stubbed token check applied to all endpoints

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import express from 'express';
 
 const app = express();
 
+app.use('/', (req, res, next) => {
+   if ( ! req.headers.token ) {
+       res.status(401).send("computer says no");
+   } else {
+       next();
+   }
+} );
+
 app.use('/books', BookController  );
 
 // handle errors, log diagnostic, give user simple error message
