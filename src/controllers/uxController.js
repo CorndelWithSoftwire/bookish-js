4801c91e70ce9af86c0d97c78f15d83e71eea763
Dave Artus <djna01@gmail.com>
2021-05-01 09:44:30 +0100

    access Book repository from UX

--- src/controllers/uxController.js
+++ src/controllers/uxController.js
@@ -2,12 +2,16 @@
 
 const express = require('express');
 const renderPage = require('../helpers/pageHelper');
+const BookRepository = require('../repositories/bookRepository');
+
 
 class UxController {
     constructor() {
         this.router = express.Router();
         this.router.get('/home', (request, response) => this.getHome(request, response));
+        this.router.get('/catalogue', (request, response) => this.getCatalogue(request, response));
         this.router.post('/', (request, response) => this.postWork(request, response));
+        this.bookRepository = new BookRepository();
     }
 
     getHome(request, response) {
@@ -18,13 +22,29 @@ class UxController {
         renderPage(request, response, "ux/html/home.html", userInfo);
     }
 
+    getCatalogue(request, response) {
+        var userInfo = {
+            user: request.bookishUser.username,
+            name: request.bookishUser.displayName
+        };
+        this.bookRepository.getAllBooks()
+            .then(books => {
+                const catalogueData = {
+                    userInfo: userInfo,
+                    books: books
+                };
+                renderPage(request, response, "ux/html/catalogue.html", catalogueData);
+            }
+            );
+    }
 
-    errorResponse(response, error, statusCode){
-        if ( ! statusCode ){
+
+    errorResponse(response, error, statusCode) {
+        if (!statusCode) {
             statusCode = 500;
         }
         response.status(statusCode).send(error);
-}
+    }
 
 }
 
