2798932a4588a7fee9d20bf2bca85e82bce7e88a
Dave Artus <djna01@gmail.com>
2021-05-25 16:21:04 +0100

    v-model separate from props

--- src/vue/js/book-details.js
+++ src/vue/js/book-details.js
@@ -3,13 +3,25 @@
 
 let BookDetails = {
     props: ['bookDetails'],
+    data: function () {
+        return {
+            activeBookDetails: {
+                author: '',
+                title: ''
+            },
+        }
+    },
+    created: function () {
+        // `this` points to the vm instance
+        Object.assign(this.activeBookDetails, this.bookDetails);
+    },
     template: `
     <div>
     <h1>Details</h1>
-    <p>Author: {{ bookDetails.author }}</p>
-    <p>Title:  {{bookDetails.title}}</p>
+    <p>Author: <input v-model="activeBookDetails.author" ></p>
+    <p>Title:  <input v-model="activeBookDetails.title"></p>
     </div>
     `
 };
 
-export {BookDetails as default };
+export { BookDetails as default };
