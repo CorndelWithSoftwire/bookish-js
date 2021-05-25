48e44d960d5c2f8803254fa98622de89fa5a5629
Dave Artus <djna01@gmail.com>
2021-05-25 10:42:10 +0100

    Click and modifier

--- src/vue/js/book-details.js
+++ src/vue/js/book-details.js
@@ -4,12 +4,20 @@
 let BookDetails = {
     props: ['bookDetails'],
     template: `
-    <div>
+    <div v-on:click="say($event)">
     <h1>Details</h1>
-    <p>Author: {{ bookDetails.author }}</p>
+    <p v-on:click.stop="displayBio">Author: {{ bookDetails.author }}</p>
     <p>Title:  {{bookDetails.title}}</p>
     </div>
-    `
+    `,
+    methods: {
+          say: function(event){
+            console.log("Say from "  + event.target.innerHTML);
+          },
+          displayBio: function(m,e){
+            console.log("Biography not avaiable ");
+          }
+    }       
 };
 
 export {BookDetails as default };
