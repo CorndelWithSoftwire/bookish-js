464a67a47a2d4cfe36c4008a367dfef103cefeb4
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-10-14 08:49:05 +0100

    add todo

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -5,30 +5,12 @@ import User from '../models/user.js';
 export default class UserRepository {
     // validate username/password
     getAuthenticatedUser(username, password) {
-        return executeSql('SELECT users.username, users.displayname  FROM users WHERE username = @username AND password = @password', 
-                                          { "username": username, "password": password} )
-            .then(result => {
-                let user = result.recordset[0];
-                if ( (!! user) ) {
-                    return new User(user.username, user.displayname);
-                } else {
-                    throw new Error('Invalid Username or Password');
-                }
-            });
+        // TODO implement check password, do this in a query, don't retrieve password
     }
 
     // is this a valid username
     getUserByName(username) {
-        return executeSql('SELECT users.username, users.displayname FROM users WHERE username = @username ' , 
-                                          { "username": username } )
-            .then(result => {
-                let user = result.recordset[0];
-                if ( (!! user) ) {
-                    return new User(user.username, user.displayname);
-                } else {
-                    throw new Error('No such user ' + username);
-                }
-            });
+          // TODO implement retrieve user info, but not the secret 
 
     }
 }
